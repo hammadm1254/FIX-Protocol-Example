f6007c58f18ba5a2fe0d52718a6ee1a73eb2b4d7
hammadm1254 <18237958+hammadm1254@users.noreply.github.com>
2018-08-27 01:29:25 -0500

    Initial parser

--- fix_main.py
+++ fix_main.py
@@ -1,11 +1,26 @@
 def getSecData(fileName):
     with open(fileName, 'r') as datFile:
-        return datFile.read()
+        data = datFile.read()
+    _output = formatSecData(data)
+    del data
+    return _output
+
+def formatSecData(secString):
+    secDataList =[]
+    for line in secString.split('\n'):
+        secDataJSON = {}
+        for tagValuePair in line.split('\x01'):
+            if len(tagValuePair) > 0:
+                tagValueList = tagValuePair.split('=')
+                secDataJSON[int(tagValueList[0])] = tagValueList[1]
+        if len(secDataJSON) > 0:
+            secDataList.append(secDataJSON)
+    return secDataList
 
 def main():
     secFileName = 'secdef.dat'
     secData = getSecData(secFileName)
-    print(secData[:500])
+    print(len(secData))
 
 if __name__ == '__main__':
     main()
