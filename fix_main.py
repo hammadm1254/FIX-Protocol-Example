4e0cc8acfa4bbad9d86e7ca7d3971f0b77fc7bde
hammadm1254 <18237958+hammadm1254@users.noreply.github.com>
2018-08-28 01:33:37 -0500

    implement tagCount with list comprehension and reduce

--- fix_main.py
+++ fix_main.py
@@ -1,11 +1,39 @@
-def getSecData(fileName):
+import functools
+def getSecTransactions(fileName):
     with open(fileName, 'r') as datFile:
-        return datFile.read()
+        data = datFile.read()
+    _output = formatSecData(data)
+    del data
+    return _output
+
+def formatSecData(secString):
+    secDataList =[]
+    for line in secString.split('\n'):
+        secDataJSON = {}
+        for tagValuePair in line.split('\x01'):
+            if len(tagValuePair) > 0:
+                tagValueList = tagValuePair.split('=')
+                secDataJSON[int(tagValueList[0])] = tagValueList[1]
+        if len(secDataJSON) > 0:
+            secDataList.append(secDataJSON)
+    return secDataList
+
+def getTagCount(tagName, tagValue, trnsxnList):
+    try:
+        tagName = int(tagName)
+    except ValueError:
+        return 'Tag names are integers'
+    try:
+        tagValue = str(tagValue)
+    except ValueError:
+        return 'Tag Values are strings'
+    result = [ aTrnsxn[tagName] == tagValue for aTrnsxn in trnsxnList]
+    return functools.reduce(lambda x,y: int(x) + int(y), result)
 
 def main():
     secFileName = 'secdef.dat'
-    secData = getSecData(secFileName)
-    print(secData[:500])
+    secTrans = getSecTransactions(secFileName)
+    print(len(secTrans))
 
 if __name__ == '__main__':
     main()
