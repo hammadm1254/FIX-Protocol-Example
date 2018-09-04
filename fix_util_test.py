ac6dd31be12f19a2526a3e74513e3f1068f06185
hammadm1254 <18237958+hammadm1254@users.noreply.github.com>
2018-09-04 03:25:32 -0500

    add more tests

--- fix_util_test.py
+++ fix_util_test.py
@@ -18,7 +18,16 @@ class TestFixUtil(unittest.TestCase):
         with self.assertRaises(TypeError):
             sln.get_defs_dict('32=f\x01555=GEH')
 
-        self.assertEqual(sln.get_defs_dict('35=d\x01555=GEH7'), {35:'d', 555:'GEH7'})
+        self.assertEqual(sln.get_defs_dict('35=d\x01555=GEH7'), {35 : 'd', 555 : 'GEH7'})
+
+    def test_verify_input(self):
+        with self.assertRaises(AssertionError):
+            sln.verify_input('23', 'GEH7', [{35 : 'd', 555 : 'GEH7'}])
+            sln.verify_input(23, 435345, [{35 : 'd', 555 : 'GEH7'}])
+            sln.verify_input(23, 'GEH7', {35: 'd', 555: 'GEH7'})
+            sln.verify_input(23, 'GEH7', [])
+
+        sln.verify_input(35, 'GEH7', [{35: 'd', 555: 'GEH7'}])
 
 if __name__ == '__main__':
     unittest.main()
